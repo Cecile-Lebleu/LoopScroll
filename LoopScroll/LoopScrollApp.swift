e0c1e6409543d115df7c7d5be41d01c5006410e4
Cécile Lebleu <cecile@cecilelebleu.com>
2024-01-05 14:12:53 -0600

    Initial Commit

---
+++ LoopScroll/LoopScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopScrollApp.swift
+//  LoopScroll
+//
+//  Created by Cécile Lebleu on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct LoopScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
